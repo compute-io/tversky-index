0523afbdeafc1b7fc4bbc8e6e6e9d03c00dc1707
Planeshifter <pburckhardt@outlook.com>
2015-02-17 23:50:28 -0500

    [UPDATE] tests added

--- test/test.js
+++ test/test.js
@@ -25,6 +25,12 @@ describe( 'compute-tversky-index', function tests() {
 
 	it( 'should throw an error if not provided two strings or two arrays', function test() {
 
+		function badValue( val1, val2 ) {
+			return function() {
+				tversky( val1, val2 );
+			};
+		}
+
 		var values = [
 			5,
 			true,
@@ -32,17 +38,28 @@ describe( 'compute-tversky-index', function tests() {
 			null,
 			NaN,
 			function(){},
-			{}
+			{},
+			[]
 		];
 
 		for ( var i = 0; i < values.length; i++ ) {
 			expect( badValue( values[i], 'a' ) ).to.throw( TypeError );
 			expect( badValue( 'a', values[i] ) ).to.throw( TypeError );
 		}
-		function badValue( val1, val2 ) {
-			return function() {
-				tversky( val1, val2 );
-			};
+
+		values = [
+			'abc',
+			true,
+			undefined,
+			null,
+			NaN,
+			function(){},
+			{},
+		];
+
+		for ( var j = 0; j < values.length; j++ ) {
+			expect( badValue( values[i], [1, 2] ) ).to.throw( TypeError );
+			expect( badValue( [1, 2], values[i] ) ).to.throw( TypeError );
 		}
 
 	});
@@ -78,7 +95,7 @@ describe( 'compute-tversky-index', function tests() {
 
 	});
 
-	it( 'should compute the Tversky similarity for default tuning params', function test() {
+	it( 'should compute the Tversky similarity of arrays for default tuning params', function test() {
 		var set1, set2, expected;
 
 		set1 = [2, 5, 7, 9];
@@ -102,5 +119,30 @@ describe( 'compute-tversky-index', function tests() {
 
 	});
 
+	it( 'should compute the Tversky similarity of strings for default tuning params', function test() {
+
+		var string1, string2, expected;
+
+		string1 = 'Harry';
+		string2 =  'Hans';
+
+		expected = 0.16666666666666666;
+
+		assert.strictEqual( tversky( string1, string2 ), expected );
+
+	});
+
+	it( 'should compute the Tversky similarity for custom tuning params', function test() {
+
+		var string1, string2, expected;
+
+		string1 = 'Harry';
+		string2 =  'Hans';
+
+		expected = 0.2857142857142857;
+
+		assert.strictEqual( tversky( string1, string2, 0.5, 0.5 ), expected );
+
+	});
 
 });
